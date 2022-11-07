6906858b002182feb960e12db33b400f0884339f
astraadmin <jettatore@yandex.ru>
2022-11-07 14:24:00 +0300

    commit

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <functional>
+
+//class B;
+//some changes
+//--amend test
+
+class A
+{
+public:
+    // A(void(*f)(int a))
+    // {
+    //     fa = f;
+    // };
+    // ~A(){};
+
+    // void(*fa)(int a);   
+    
+    // A(void(B::*f)(int i) const, const B & b)
+    // {
+    //     std::cout << "f ";
+    //     fa = f;
+    //     faa(b);
+    // }
+
+    A(std::function<void(int i)>f)
+    {
+        std::cout << "ctor\n";
+        faf = f;
+        faf(789);
+    }
+    
+    ~A() {
+        std::cout << "dctor\n";
+    }
+
+    // void(B::*fa)(int i) const;
+    std::function<void(int i)>faf;
+
+    // void faa(const B & obj)
+    // {
+    //     (obj.*fa)(123);
+    // }
+
+    void faaf()
+    {
+        faf(456);
+    }
+
+};
+
+class B
+{
+public:
+    // B(void(*f)(int a)) : a(f)
+    // {};
+    // B() : a(&B::boo, *this)
+    // {
+        
+    // };
+
+    B()// : a(std::bind(&B::boo, *this, std::placeholders::_1))
+    {
+        std::cout << "+++" << std::endl;
+        a = new A(std::bind(&B::boo, *this, std::placeholders::_1));
+        std::cout << "---" << std::endl;
+    };
+
+    ~B() { delete a; };
+
+    A* a = nullptr;
+
+    static void foo(int i);
+
+private:
+    void boo(int i) const;    
+};
+
+void B::boo(int i) const
+{
+     std::cout << "B::boo: " << i << std::endl;
+}
+
+void B::foo(int i)
+{
+    std::cout << "B::foo, " << i << std::endl;
+}
+
+int main()
+{
+    // B b(boo);
+    // b.a.fa(666);
+    B b;
+    
+    return 0;
+}
